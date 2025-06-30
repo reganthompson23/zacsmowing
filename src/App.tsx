a6faa5a379d630ab3e7d97aded79e7b75bf73e54
reganthompson23 <reganthompson23>
2025-06-30 18:58:28 +1000

    Update service area banner: Change to Moffatdale and improve mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -181,8 +181,10 @@ function App() {
       {/* Add padding to account for fixed header */}
       <div className="pt-20">
         {/* Location Banner */}
-        <div className="bg-green-50 text-green-800 py-2 text-center font-medium border-b border-green-100">
-          Servicing Murgon to Kingaroy, based out of Wondai
+        <div className="bg-green-50 text-green-800 py-2 px-4 text-center font-medium border-b border-green-100">
+          <span className="inline-block leading-snug">
+            Servicing Moffatdale to Kingaroy,<br className="sm:hidden" /> based out of Wondai
+          </span>
         </div>
 
         {currentPage === 'gallery' ? (
